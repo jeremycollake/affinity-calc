113b1caf67b0a7523da7d60b192e92ee48b20e20
jeremy <jeremy@bitsum.com>
2019-09-02 06:07:09 -0400

    complete return output instead of emit

--- affinity-calc.php
+++ affinity-calc.php
@@ -4,19 +4,20 @@ function cpu_affinity_calculator($atts) {
 	$rows = shortcode_atts(array('rows' => '8',), $atts)["rows"];	
 	
 	$dir=str_replace($_SERVER['DOCUMENT_ROOT'], "", __DIR__);
-    echo "<script src=\"$dir/affinity-calc.js\" type=\"text/javascript\"></script>";
-	$table="<table>";
+    $ret="<script src=\"$dir/affinity-calc.js\" type=\"text/javascript\"></script>";
+	$ret.="<table>";
 
 	for($cur_row=0;$cur_row<$rows;$cur_row++) {
-		$table.="<tr>";
+		$ret.="<tr>";
 		for($cur_col=0;$cur_col<(64/$rows);$cur_col++) {
 			$cpu=($cur_col*$rows)+$cur_row;
-			$table.=sprintf("<td><input type=\"checkbox\" id=\"cpu%d\" name=\"cpu%d\" onclick=\"cpuCheck_event()\" class=\"cpu_selector\"> <label for=\"cpu%d\">CPU %d</label></td>", $cpu, $cpu, $cpu, $cpu);
+			$ret.=sprintf("<td><input type=\"checkbox\" id=\"cpu%d\" name=\"cpu%d\" onclick=\"cpuCheck_event()\" class=\"cpu_selector\"> <label for=\"cpu%d\">CPU %d</label></td>", $cpu, $cpu, $cpu, $cpu);
 		}
-		$table.="</tr>";
+		$ret.="</tr>";
 	}
-	$table.="</table>";	
-	return $table . "<label for=\"mask_hex\">CPU Affinity Bitmask (hex):</label> <input type=\"text\" size=\"26\" id=\"mask_hex\" name=\"mask_hex\" onchange=\"cpuMask_event()\">";
+	$ret.="</table>";
+	$ret.="<label for=\"mask_hex\">CPU Affinity Bitmask (hex):</label> <input type=\"text\" size=\"26\" id=\"mask_hex\" name=\"mask_hex\" onchange=\"cpuMask_event()\">";
+	return $ret;
 }
 
 ?>
