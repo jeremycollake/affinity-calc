76b0af183acc0df9b4d77553ec024981cb65cbaa
jeremycollake <jeremy@bitsum.com>
2021-09-09 10:07:52 -0400

    Refactor to a Javascript class

--- affinity-calc.js
+++ affinity-calc.js
@@ -1,63 +1,65 @@
-function pad0(num, width) {
-    let zeros;
-    for (let i = 0; i < width; i++) {
-        zeros += "0";
+class AffinityCalc {
+    static pad0(num, width) {
+        let zeros;
+        for (let i = 0; i < width; i++) {
+            zeros += "0";
+        }
+        return (zeros + num).substr(-width);
     }
-    return (zeros + num).substr(-width);
-}
 
-function long2hex(num) {
-    return (pad0((num >>> 24).toString(16), 2) +
-        pad0((num >> 16 & 255).toString(16), 2) +
-        pad0((num >> 8 & 255).toString(16), 2) +
-        pad0((num & 255).toString(16), 2));
-}
+    static long2hex(num) {
+        return (this.pad0((num >>> 24).toString(16), 2) +
+            this.pad0((num >> 16 & 255).toString(16), 2) +
+            this.pad0((num >> 8 & 255).toString(16), 2) +
+            this.pad0((num & 255).toString(16), 2));
+    }
 
-function cpuCheck_event() {
-    let maskLow = 0;
-    let maskHigh = 0;
-    for (let i = 0; i < 32; i++) {
-        if (document.getElementById("cpu" + i).checked) {
-            maskLow |= (1 << i);
+    static mark_cpus_16bit(word, iBegin) {
+        word = parseInt(word, 16);
+        for (let i = 0; i < 16; i++) {
+            $cpuElem = document.getElementById("cpu" + (iBegin + i));
+            $cpuElem.checked = (word >>> i) & 1;
         }
     }
-    for (let i = 32; i < 64; i++) {
-        if (document.getElementById("cpu" + i).checked) {
-            maskHigh |= (1 << i);
+
+    static cpuCheck_event() {
+        let maskLow = 0;
+        let maskHigh = 0;
+        for (let i = 0; i < 32; i++) {
+            if (document.getElementById("cpu" + i).checked) {
+                maskLow |= (1 << i);
+            }
         }
+        for (let i = 32; i < 64; i++) {
+            if (document.getElementById("cpu" + i).checked) {
+                maskHigh |= (1 << i);
+            }
+        }
+        document.getElementById("mask-hex").value = "0x" + this.long2hex(maskHigh).toUpperCase() + this.long2hex(maskLow).toUpperCase();
     }
-    document.getElementById("mask-hex").value = "0x" + long2hex(maskHigh).toUpperCase() + long2hex(maskLow).toUpperCase();
-}
 
-function mark_cpus_16bit(word, iBegin) {
-    word = parseInt(word, 16);
-    for (let i = 0; i < 16; i++) {
-        $cpuElem = document.getElementById("cpu" + (iBegin + i));
-        $cpuElem.checked = (word >>> i) & 1;
+    static cpuMask_event() {
+        let mask = this.pad0(document.getElementById("mask-hex").value.toUpperCase().replace("0X", ""), 16);
+        let maskHigh = mask.substr(0, 8);
+        let maskLow = mask.substr(8, 8);
+
+        this.mark_cpus_16bit(maskLow.substr(4, 4), 0);
+        this.mark_cpus_16bit(maskLow.substr(0, 4), 16);
+        this.mark_cpus_16bit(maskHigh.substr(4, 4), 32);
+        this.mark_cpus_16bit(maskHigh.substr(0, 4), 48);
     }
-}
 
-function cpuMask_event() {
-    let mask = pad0(document.getElementById("mask-hex").value.toUpperCase().replace("0X", ""), 16);
-    let maskHigh = mask.substr(0, 8);
-    let maskLow = mask.substr(8, 8);
-
-    mark_cpus_16bit(maskLow.substr(4, 4), 0);
-    mark_cpus_16bit(maskLow.substr(0, 4), 16);
-    mark_cpus_16bit(maskHigh.substr(4, 4), 32);
-    mark_cpus_16bit(maskHigh.substr(0, 4), 48);
-}
-
-function invert_event() {
-    for (const cpu of document.getElementsByClassName("cpu-checkbox")) {
-        cpu.checked = !cpu.checked;
+    static invert_event() {
+        for (const cpu of document.getElementsByClassName("cpu-checkbox")) {
+            cpu.checked = !cpu.checked;
+        }
+        this.cpuCheck_event();
     }
-    cpuCheck_event();
-}
 
-function clear_event() {
-    for (const cpu of document.getElementsByClassName("cpu-checkbox")) {
-        cpu.checked = false;
+    static clear_event() {
+        for (const cpu of document.getElementsByClassName("cpu-checkbox")) {
+            cpu.checked = false;
+        }
+        this.cpuCheck_event();
     }
-    cpuCheck_event();
 }
--- affinity-calc.php
+++ affinity-calc.php
@@ -4,22 +4,22 @@ function cpu_affinity_calculator($atts) {
 	$rows = shortcode_atts(array('rows' => '8',), $atts)["rows"];
 
 	$dir = str_replace($_SERVER['DOCUMENT_ROOT'], "", __DIR__);
-	$ret = "<script src=\"$dir/affinity-calc.js?ver=09072021000\" type=\"text/javascript\"></script>"
+	$ret = "<script src=\"$dir/affinity-calc.js?ver=09092021000\" type=\"text/javascript\"></script>"
 		. "<table class=\"table table-striped\">";
 
 	for($cur_row=0;$cur_row<$rows;$cur_row++) {
 		$ret .= "<tr>";
 		for($cur_col=0;$cur_col<(64/$rows);$cur_col++) {
 			$cpu = ($cur_col*$rows)+$cur_row;
-			$ret .= sprintf("<td><input type=\"checkbox\" id=\"cpu%d\" name=\"cpu%d\" onclick=\"cpuCheck_event()\" class=\"cpu-checkbox\"> <label for=\"cpu%d\">CPU %d</label></td>", $cpu, $cpu, $cpu, $cpu);
+			$ret .= sprintf("<td><input type=\"checkbox\" id=\"cpu%d\" name=\"cpu%d\" onclick=\"AffinityCalc.cpuCheck_event()\" class=\"cpu-checkbox\"> <label for=\"cpu%d\">CPU %d</label></td>", $cpu, $cpu, $cpu, $cpu);
 		}
 		$ret .= "</tr>";
 	}
 	$ret .= "</table>"
-		. "<input type=\"button\" value=\"invert\" id=\"btnInvert\" onclick=\"invert_event()\">"
-		. "<input type=\"button\" value=\"clear\" id=\"btnClear\" onclick=\"clear_event()\">"
+		. "<input type=\"button\" value=\"invert\" id=\"btnInvert\" onclick=\"AffinityCalc.invert_event()\">"
+		. "<input type=\"button\" value=\"clear\" id=\"btnClear\" onclick=\"AffinityCalc.clear_event()\">"
 		. "<br />"
-		. "<label for=\"mask-hex\">CPU Affinity Bitmask (hex):</label> <input type=\"text\" size=\"26\" id=\"mask-hex\" name=\"mask-hex\" onchange=\"cpuMask_event()\">";
+		. "<label for=\"mask-hex\">CPU Affinity Bitmask (hex):</label> <input type=\"text\" size=\"26\" id=\"mask-hex\" name=\"mask-hex\" onchange=\"AffinityCalc.cpuMask_event()\">";
 	return $ret;
 }
 ?>
